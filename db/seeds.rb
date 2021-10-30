e862aa63075520e0002721a198bd3a348d3b0107
Bossanov <31960270+Bossanov@users.noreply.github.com>
2021-10-30 10:07:43 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,27 @@
-50.times do
-Article.create([{title: Faker::Artist.name, body: Faker::Lorem.paragraphs , theme: [:theme1, :theme2,:theme3,:theme4].sample}])
-end
+#50.times do
+#Article.create([{title: Faker::Artist.name, body: Faker::Lorem.paragraphs , theme: [:theme1, :theme2,:theme3,:theme4].sample}])
+#end
+
+Profile.destroy_all
+User.destroy_all
+
+puts "***--- ADMIN #1 CREATION ---***"
+user = User.create!(
+    email: "staumont.antoine@me.com",
+    password: "Bossanova1",
+
+
+)
+  prof = Profile.create!(
+   first_name: "Antoine",
+    last_name: "STAUMONT",
+
+    statut: "admin",
+   address: "277 Avenue Jean Garaialde",
+    post_code: "62152",
+    city: "HARDELOT",
+    country: "France",
+    user_id: user.id,
+  )
+
+
